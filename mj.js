a63bd7c5fae12a67e7f4fe926397210b4d5d32ef
lorianebarbosa <75947302+lorianebarbosa@users.noreply.github.com>
2020-12-13 12:59:47 -0500

    Add files via upload
    initial revision

---
+++ mj.js
@@ -0,0 +1,6 @@
+var rainbowSubtitle = document.getElementById("rainbowSubtitle")
+function changeBackground(){
+ console.log("Makestringdark")//* about to make string darker
+rainbowSubtitle.style.background = "red"
+}
+rainbowSubtitle.addEventListener("click",changeBackground)
